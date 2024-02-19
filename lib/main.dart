4ebedcbc14fbfbb61244990b466785327c5319fd
Agusti Gunawan <agustigunawan10@gmail.com>
2024-02-20 02:27:23 +0900

    Revert "add flash page"
    This reverts commit d9e40dc48a7777414cf3c13928fc4c0cbaba7f29.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:omotenashi_cafe/presentation/pages/flash_page/flash_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +10,11 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: FlashPage(),
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
     );
   }
 }
